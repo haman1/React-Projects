30fbc91a600812b43c547eb816ebf8dec7f2a652
Ombasa Herman <ombasaherman@gmail.com>
2023-01-03 16:03:02 +0300

    pages

---
+++ cart/src/pages/checkout/Checkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './checkout.css'
+function checkout() {
+  return (
+    <h2>Thank you for Shopping with us</h2>
+  )
+}
+
+export default checkout
---
+++ cart/src/pages/shop/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="container">
+        <h1><span>Karl</span>-Kani</h1>
+            <p>&copy; Copyright 2022</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
