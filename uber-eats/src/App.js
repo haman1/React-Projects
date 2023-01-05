64593105ab73eb04bcea93eef3b73885aaf71615
Ombasa Herman <ombasaherman@gmail.com>
2023-01-05 22:24:25 +0300

    App.js

---
+++ uber-eats/src/App.js
@@ -0,0 +1,18 @@
+import Category from "./components/Category";
+import Foods from "./components/Foods";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Foods/>
+      <Category />
+    </div>
+  );
+}
+
+export default App;
